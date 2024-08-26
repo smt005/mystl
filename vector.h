1db91c90044753b98153af8d9b07442f2636284d
Коптяев Александр <a.koptyaev@playrix.com>
2024-08-27 02:05:31 +0600

    Прототип vector

--- vector.h
+++ vector.h
@@ -1,6 +1,55 @@
 #pragma once
 
-namespace mystl {
+namespace mystd {
+	template <typename T>
 	class vector final {
+	public:
+		vector() = default;
+
+		T& emplace_back(T&& value)
+		{
+			if (_size == _capacity) {
+				size_t newCapacity = (_size * 2) + 1;
+				T* tempData = new T[newCapacity];
+
+				if (std::is_integral<T>()) {
+					memcpy(tempData, _data, sizeof(T) * _size);
+				}
+				else {
+					for (int i = 0; i < _size; ++i) {
+						tempData[i] = _data[i];
+					}
+				}
+
+				std::swap(_data, tempData);
+				delete[] tempData;
+
+				_capacity = newCapacity;
+			}
+
+			size_t lastIndex = _size++;
+			_data[lastIndex] = value;			
+			return _data[lastIndex];
+		}
+
+		T& operator[](size_t index)
+		{
+			return _data[index];
+		}
+
+		const T& operator[](size_t index) const 
+		{
+			return _data[index];
+		}
+
+		size_t size() const
+		{
+			return _size;
+		}
+
+	private:
+		T* _data = nullptr;
+		size_t _size = 0;
+		size_t _capacity = 0;
 	};
 }
