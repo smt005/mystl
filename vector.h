1d79408cf6173d55575df4b052e05df87f96f77c
Коптяев Александр <a.koptyaev@playrix.com>
2024-08-29 03:04:26 +0600

    Emplacer

--- vector.h
+++ vector.h
@@ -23,22 +23,60 @@ namespace mystd {
 					return;
 				}
 
+				AllocT allocator;
+
 				if (!std::is_trivially_copyable<T>::value) {
 					for (size_t index = 0; index < size; ++index) {
-						data[index].~T();
+						std::allocator_traits<AllocT>::destroy(allocator, &data[index]);
 					}
 					if (newIndex != std::numeric_limits<size_t>::max()) {
-						data[newIndex].~T();
+						std::allocator_traits<AllocT>::destroy(allocator, &data[newIndex]);
 					}
 				}
 
-				AllocT allocator;
 				allocator.deallocate(data, capacity);
+			}
 
-				data = nullptr;
-				capacity = 0;
-				size = 0;
-				newIndex = std::numeric_limits<size_t>::max();
+			static void Emplace(T* dest, T* source, size_t size) {
+				struct Emplacer {
+					T* dest = nullptr;
+					T* source = nullptr;
+					size_t emp_size = 0;
+
+					Emplacer(T* _dest, T* _source)
+						: dest(_dest)
+						, source(_source)
+					{
+					}
+
+					void Emplace(T&& arg) {
+						new(&dest[emp_size]) T(std::forward<T>(arg));
+						//std::allocator_traits<AllocT>::
+						++emp_size;
+					}
+
+					~Emplacer() {
+						AllocT allocator;
+						for (size_t index = 0; index < emp_size; ++index) {
+							std::allocator_traits<AllocT>::destroy(allocator, &dest[index]);
+						}
+					}
+
+					void Reset() {
+						emp_size = 0;
+					}
+				};
+
+				if (std::is_trivially_copyable<T>::value) {
+					memcpy(dest, source, sizeof(T) * size);
+				}
+				else {
+					Emplacer emplacer(dest, source);
+					for (size_t index = 0; index < size; ++index) {
+						emplacer.Emplace(std::forward<T>(source[index]));
+					}
+					emplacer.Reset();
+				}
 			}
 		};
 
@@ -56,22 +94,8 @@ namespace mystd {
 				new(&tempContainer->data[_container->size]) T(arg);
 				tempContainer->newIndex = _container->size;
 
-				if (std::is_trivially_copyable<T>::value) {
-					memcpy(tempContainer->data, _container->data, sizeof(T) * _container->size);
-					tempContainer->size = _container->size;
-				}
-				else if (std::is_nothrow_move_constructible<T>::value) {
-					for (size_t index = 0; index < _container->size; ++index) {
-						new(&tempContainer->data[index]) T(std::forward<T&&>(_container->data[index]));
-						++tempContainer->size;
-					}
-				}
-				else {
-					for (size_t index = 0; index < _container->size; ++index) {
-						new(&tempContainer->data[index]) T(_container->data[index]);
-						++tempContainer->size;
-					}
-				}
+				Container::Emplace(tempContainer->data, _container->data, _container->size);
+				tempContainer->size = _container->size;
 
 				tempContainer->size = tempContainer->newIndex + 1;
 				tempContainer->newIndex = std::numeric_limits<size_t>::max();
